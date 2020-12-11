3ad45e6ecc37bf245404a29c5e129076d3af45dc
Firdavs Hasanov <firdavs_h2@outlook.com>
2020-12-11 15:55:04 -0500

    added Like, Comment & Rate populator for Picture object. Added Favorite list get method

--- java/src/main/java/com/techelevator/dao/PicServiceSqlDAO.java
+++ java/src/main/java/com/techelevator/dao/PicServiceSqlDAO.java
@@ -2,9 +2,12 @@ package com.techelevator.dao;
 
 
 
+import java.util.List;
+
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 import com.techelevator.model.Ratings;
@@ -84,6 +87,44 @@ public class PicServiceSqlDAO implements PicServiceDAO{
 			throw new DataAccessResourceFailureException("Can not reach database " + e.getMessage());
 		}
 		return null;
+	}
+	
+	
+	//helpers
+	public List<Likes> getLikesByPicId(int pictureId){
+		String sql ="SELECT like_id, picture_id, l.user_id, u.username FROM likes l "
+				+ "JOIN users u ON u.user_id=l.user_id WHERE l.picture_id=?";
+		SqlRowSet rowSet; 
+		try {
+			 rowSet= jdbcTemplate.queryForRowSet(sql, pictureId);
+		} catch (DataAccessException e) {
+			throw new DataAccessResourceFailureException("Can not reach database " + e.getMessage());
+		}
+		return RowMapper.mapRowsetToLikeList(rowSet);
+	}
+	
+	public List<Comments> getCommentsByPicId(int pictureId){
+		String sql ="SELECT comment_id, picture_id, c.user_id, comment, u.username FROM comments c "
+				+ "JOIN users u ON u.user_id=c.user_id WHERE c.picture_id=?";
+		SqlRowSet rowSet; 
+		try {
+			 rowSet= jdbcTemplate.queryForRowSet(sql, pictureId);
+		} catch (DataAccessException e) {
+			throw new DataAccessResourceFailureException("Can not reach database " + e.getMessage());
+		}
+		return RowMapper.mapRowsetToCommentList(rowSet);
+	}
+	
+	public List<Ratings> getRatingsByPicId(int pictureId){
+		String sql ="SELECT rating_id, picture_id, r.user_id, rating, u.username FROM ratings r "
+				+ "JOIN users u ON u.user_id=r.user_id WHERE r.picture_id=?";
+		SqlRowSet rowSet; 
+		try {
+			 rowSet= jdbcTemplate.queryForRowSet(sql, pictureId);
+		} catch (DataAccessException e) {
+			throw new DataAccessResourceFailureException("Can not reach database " + e.getMessage());
+		}
+		return RowMapper.mapRowsetToRatingList(rowSet);
 	}
 
 }
