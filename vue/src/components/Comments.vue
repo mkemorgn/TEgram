c70bf364604072641583b0a3e6e1f59a46e4dbb3
Mike Morgan <morgan.mike55@gmail.com>
2020-12-12 15:48:24 -0500

    changes

---
+++ vue/src/components/Comments.vue
@@ -0,0 +1,13 @@
+<template>
+  <form class="new-comment" v-on:submit.prevent="postComment" autocomplete="off">
+    <input
+      type="text"
+      name="comment"
+      id="comment"
+      placeholder="Add a comment..."
+      v-model="comment"
+    />
+    <button v-bind:disabled="!comment">Post</button>
+  </form>
+</template>
+
