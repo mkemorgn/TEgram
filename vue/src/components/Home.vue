6ce5e66b3679e189deda27eb74f690c4d1f6a0ef
Plaksina <olgaplaksina@yahoo.com>
2020-12-12 21:40:02 -0500

    merge conflicts

---
+++ vue/src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <photo-feed v-bind:photos="this.$store.state.photos" />
+</template>
+
+<script>
+import PhotoFeed from "./PhotoFeed.vue";
+export default {
+  components: { PhotoFeed },
+};
+</script>
