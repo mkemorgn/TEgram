02c2a87d15b79f5e4003e1e013f26d8d3b050029
Student <student@Techs-MacBook-Air.local>
2020-12-15 14:06:03 -0500

    commit after big pull

--- vue/src/views/Profile.vue
+++ vue/src/views/Profile.vue
@@ -1,12 +1,51 @@
 <template>
   <div class="Profile">
-    <h1>Profile</h1>
-    <p>Your profile here.</p>
+    <h1>Profile - Pictures</h1>
+    <photo-feed
+      v-bind:photos="this.$store.state.photos"
+      v-bind:pageLoaded="this.pageLoaded"
+    />
   </div>
 </template>
 
 <script>
+import PhotoService from "@/services/PhotoService";
+import PhotoFeed from "../components/PhotoFeed";
+
 export default {
-  name: "profile"
+  name: "profile",
+  components: {
+    PhotoFeed,
+  },
+  data() {
+    return {
+      pageLoaded: false,
+    };
+  },
+  created() {
+    this.retrievePhotos();
+  },
+  methods: {
+    retrievePhotos() {
+      PhotoService.getUserPhotos()
+        .then((response) => {
+          this.$store.commit("SET_PHOTOS", response.data);
+          this.pageLoaded = true;
+        })
+        .catch((error) => {
+          if (error.response) {
+            this.errorMsg =
+              "Error. Response received was '" +
+              error.response.statusText +
+              "'.";
+          } else if (error.request) {
+            this.errorMsg = "Error. Server could not be reached.";
+          } else {
+            this.errorMsg = "Error. Request could not be created.";
+          }
+        });
+    },
+  },
 };
 </script>
+
