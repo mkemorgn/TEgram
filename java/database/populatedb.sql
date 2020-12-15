b0084754122a10482d900e6b41781a123b79c602
Firdavs Hasanov <firdavs_h2@outlook.com>
2020-12-15 12:57:02 -0500

    cardinal changes on DB and Server side for favorites

--- java/database/populatedb.sql
+++ java/database/populatedb.sql
@@ -4,65 +4,26 @@ INSERT INTO public.users ( username, password_hash, role, activated, deleted) VA
 INSERT INTO public.users ( username, password_hash, role, activated, deleted) VALUES ( 'user2', '$2a$10$z.ZfdTwrjZkadjTKZIgD5OrM9ovN2QacC75UTX1E/vEttq8Qh9D.C', 'ROLE_USER', true, false);
 INSERT INTO public.users ( username, password_hash, role, activated, deleted) VALUES ( 'user3', '$2a$10$EciTGklE6FirjBtwa9ApOONEbCemuS9GwaxgrvypP/RrGfcRto95W', 'ROLE_USER', true, false);
 
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 3, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607573500/3/yi2wwpz37sedzb8eujko.jpg', 'sheep.jpg', '3/yi2wwpz37sedzb8eujko', 'my sheeps', false);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 3, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607573532/3/mwaa6n4pmzdpcyypeule.jpg', 'bike.jpg', '3/mwaa6n4pmzdpcyypeule', 'red bike', false);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 3, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574059/3/zgqnlklydhkrppoomy1y.jpg', 'bicycle.jpg', '3/zgqnlklydhkrppoomy1y', 'red bike', false);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 3, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574078/3/xqmz9fieg8d1conmztsk.jpg', 'boy-snow-hoodie.jpg', '3/xqmz9fieg8d1conmztsk', '', true);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 3, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574091/3/ngihonkghsvthrth2lll.jpg', 'jazz.jpg', '3/ngihonkghsvthrth2lll', '', true);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 4, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574173/4/waqhjmnhecqtuyomih9c.jpg', 'kitchen-bar.jpg', '4/waqhjmnhecqtuyomih9c', 'cook', false);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 4, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574191/4/nfpfdlpl2ybm8r6vzmdu.jpg', 'smiling-man.jpg', '4/nfpfdlpl2ybm8r6vzmdu', '', false);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 4, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574227/4/xz7fjwvhu2bojdtwyorx.jpg', 'nature-mountains.jpg', '4/xz7fjwvhu2bojdtwyorx', '', false);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 4, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574237/4/g1hcv2ltr2bc7tq38gbv.jpg', 'girl-urban-view.jpg', '4/g1hcv2ltr2bc7tq38gbv', '', false);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 4, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574282/4/bfu0tdnvlbvr9i42e4wo.jpg', 'architecture-signs.jpg', '4/bfu0tdnvlbvr9i42e4wo', '', true);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 4, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574357/4/zmmo04gwgvdheou28pkp.jpg', 'beach-boat.jpg', '4/zmmo04gwgvdheou28pkp', '', false);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 5, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574392/5/icv72lcynltpie0ahzwh.jpg', 'dessert.jpg', '5/icv72lcynltpie0ahzwh', '', false);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 5, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574424/5/rc5sotwydcrtea3ouhtw.jpg', 'fish-vegetables.jpg', '5/rc5sotwydcrtea3ouhtw', 'fish', false);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 5, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574451/5/fimjg7c4iedjfzkzzryg.jpg', 'pot-mussels.jpg', '5/fimjg7c4iedjfzkzzryg', 'love mussels', false);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 5, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574463/5/ev27myglfexomdwjp5pd.jpg', 'spices.jpg', '5/ev27myglfexomdwjp5pd', '', true);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 1, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574486/1/ebnrutmrrhaa8d7fpqyr.jpg', 'cat.jpg', '1/ebnrutmrrhaa8d7fpqyr', '', false);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 1, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574533/1/bdhrxjcitqrwojsvlzod.jpg', 'reindeer.jpg', '1/bdhrxjcitqrwojsvlzod', 'deer', false);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 1, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574554/1/tuarjpgb2e1bkqhzk1ho.jpg', 'three-dogs.jpg', '1/tuarjpgb2e1bkqhzk1ho', 'my dogs', true);
-INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private) VALUES ( 1, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607658371/6/otwrz1srklxenfwd83pp.png', 'Titatnic.jpg', '1/tuarjpgb2e1bkqhzk1ho', 'my dogs', true);
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 3, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607573500/3/yi2wwpz37sedzb8eujko.jpg', 'sheep.jpg', '3/yi2wwpz37sedzb8eujko', 'my sheeps', false, false );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 3, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607573532/3/mwaa6n4pmzdpcyypeule.jpg', 'bike.jpg', '3/mwaa6n4pmzdpcyypeule', 'red bike', false, true );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 3, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574059/3/zgqnlklydhkrppoomy1y.jpg', 'bicycle.jpg', '3/zgqnlklydhkrppoomy1y', 'red bike', false, true );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 3, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574078/3/xqmz9fieg8d1conmztsk.jpg', 'boy-snow-hoodie.jpg', '3/xqmz9fieg8d1conmztsk', '', true, true );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 3, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574091/3/ngihonkghsvthrth2lll.jpg', 'jazz.jpg', '3/ngihonkghsvthrth2lll', '', true, false );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 4, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574173/4/waqhjmnhecqtuyomih9c.jpg', 'kitchen-bar.jpg', '4/waqhjmnhecqtuyomih9c', 'cook', false, false );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 4, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574191/4/nfpfdlpl2ybm8r6vzmdu.jpg', 'smiling-man.jpg', '4/nfpfdlpl2ybm8r6vzmdu', '', false, true );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 4, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574227/4/xz7fjwvhu2bojdtwyorx.jpg', 'nature-mountains.jpg', '4/xz7fjwvhu2bojdtwyorx', '', false, true );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 4, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574237/4/g1hcv2ltr2bc7tq38gbv.jpg', 'girl-urban-view.jpg', '4/g1hcv2ltr2bc7tq38gbv', '', false, true );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 4, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574282/4/bfu0tdnvlbvr9i42e4wo.jpg', 'architecture-signs.jpg', '4/bfu0tdnvlbvr9i42e4wo', '', true, true );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 4, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574357/4/zmmo04gwgvdheou28pkp.jpg', 'beach-boat.jpg', '4/zmmo04gwgvdheou28pkp', '', false, false );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 5, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574392/5/icv72lcynltpie0ahzwh.jpg', 'dessert.jpg', '5/icv72lcynltpie0ahzwh', '', false, true );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 5, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574424/5/rc5sotwydcrtea3ouhtw.jpg', 'fish-vegetables.jpg', '5/rc5sotwydcrtea3ouhtw', 'fish', false, true );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 5, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574451/5/fimjg7c4iedjfzkzzryg.jpg', 'pot-mussels.jpg', '5/fimjg7c4iedjfzkzzryg', 'love mussels', false, false );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 5, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574463/5/ev27myglfexomdwjp5pd.jpg', 'spices.jpg', '5/ev27myglfexomdwjp5pd', '', true, true );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 1, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574486/1/ebnrutmrrhaa8d7fpqyr.jpg', 'cat.jpg', '1/ebnrutmrrhaa8d7fpqyr', '', false, false );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 1, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574533/1/bdhrxjcitqrwojsvlzod.jpg', 'reindeer.jpg', '1/bdhrxjcitqrwojsvlzod', 'deer', false, true );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 1, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607574554/1/tuarjpgb2e1bkqhzk1ho.jpg', 'three-dogs.jpg', '1/tuarjpgb2e1bkqhzk1ho', 'my dogs', true,true );
+INSERT INTO public.pictures ( user_id, pic_url, pic_name, pic_server_name, description, private, favorite ) VALUES ( 1, 'https://res.cloudinary.com/dr1xyxwvj/image/upload/v1607658371/6/otwrz1srklxenfwd83pp.png', 'Titatnic.jpg', '1/tuarjpgb2e1bkqhzk1ho', 'my dogs', true, false );
 
-
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'my list', 3);
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'favorite', 4);
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'looks good', 5);
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'picture', 3);
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'nice', 3);
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'interesting', 5);
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'fantastic', 4);
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'great', 4);
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'groovy', 5);
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'mesmerizing', 5);
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'nice shot', 3);
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'slay', 4);
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'masterpiece', 5);
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'goodness', 3);
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'well-done', 4);
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'game time', 3);
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'outlook', 4);
-INSERT INTO public.favorites ( fav_name, user_id) VALUES ( 'another one', 4);
-
-
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (1, 1);
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (1, 2);
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (1, 3);
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (1, 4);
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (2, 18);
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (3, 10);
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (6, 11);
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (4, 12);
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (5, 13);
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (7, 14);
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (8, 15);
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (9, 16);
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (10, 9);
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (11, 17);
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (18, 8);
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (17, 7);
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (16, 6);
-INSERT INTO public.favorite_picture (favorite_id, picture_id) VALUES (5, 5);
 
 
 INSERT INTO public.ratings ( picture_id, user_id, rating) VALUES ( 1, 2, 5);
--- java/src/main/java/com/techelevator/model/Picture.java
+++ java/src/main/java/com/techelevator/model/Picture.java
@@ -12,16 +12,14 @@ public class Picture {
 	private String picName;
 	private String description;
 	private boolean isPrivate;
+	private boolean favorite;
 	private List<Comments> comments;
 	private List<Likes> likes;
 	private List<Ratings> ratings;
 	
-	
-	
-
 	public Picture(int pictureId, int userId, String userName, String picUrl, String picServerName, String picName,
-			String description, boolean isPrivate) {
-		
+			String description, boolean isPrivate, boolean favorite ) {
+	
 		this.pictureId = pictureId;
 		this.userId = userId;
 		this.userName = userName;
@@ -30,33 +28,84 @@ public class Picture {
 		this.picName = picName;
 		this.description = description;
 		this.isPrivate = isPrivate;
+		this.favorite = favorite;
 		this.comments = null;
 		this.likes = null;
 		this.ratings = null;
 	}
 
-
-	public Picture () {
-		
+	public int getPictureId() {
+		return pictureId;
 	}
-	
-	
+
+	public void setPictureId(int pictureId) {
+		this.pictureId = pictureId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
 	public String getUserName() {
 		return userName;
 	}
-
 
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
 
+	public String getPicUrl() {
+		return picUrl;
+	}
 
-	public List<Ratings> getRatings() {
-		return ratings;
+	public void setPicUrl(String picUrl) {
+		this.picUrl = picUrl;
 	}
-	public void setRatings(List<Ratings> ratings) {
-		this.ratings = ratings;
+
+	public String getPicServerName() {
+		return picServerName;
 	}
+
+	public void setPicServerName(String picServerName) {
+		this.picServerName = picServerName;
+	}
+
+	public String getPicName() {
+		return picName;
+	}
+
+	public void setPicName(String picName) {
+		this.picName = picName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isPrivate() {
+		return isPrivate;
+	}
+
+	public void setPrivate(boolean isPrivate) {
+		this.isPrivate = isPrivate;
+	}
+
+	public boolean isFavorite() {
+		return favorite;
+	}
+
+	public void setFavorite(boolean favorite) {
+		this.favorite = favorite;
+	}
+
 	public List<Comments> getComments() {
 		return comments;
 	}
@@ -73,52 +122,15 @@ public class Picture {
 		this.likes = likes;
 	}
 
+	public List<Ratings> getRatings() {
+		return ratings;
+	}
 
-	public int getPictureId() {
-		return pictureId;
-	}
-	public void setPictureId(int pictureId) {
-		this.pictureId = pictureId;
-	}
-	public int getUserId() {
-		return userId;
-	}
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-	public String getPicUrl() {
-		return picUrl;
-	}
-	public void setPicUrl(String picUrl) {
-		this.picUrl = picUrl;
-	}
-	public String getPicServerName() {
-		return picServerName;
-	}
-	public void setPicServerName(String picServerName) {
-		this.picServerName = picServerName;
-	}
-	public String getPicName() {
-		return picName;
-	}
-	public void setPicName(String picName) {
-		this.picName = picName;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public boolean isPrivate() {
-		return isPrivate;
-	}
-	public void setPrivate(boolean isPrivate) {
-		this.isPrivate = isPrivate;
+	public void setRatings(List<Ratings> ratings) {
+		this.ratings = ratings;
 	}
 	
 	
-
-	
-
 }
+	
+	
